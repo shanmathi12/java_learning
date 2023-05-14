359e099e6b8263ca9317a40031d32ab8446d2b01
shanmathi <shanmathisaravanan02@gmail.com>
2023-05-14 13:13:28 -0400

    hello world source code

---
+++ src/basics/Helloworld1.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class Helloworld1 {
+	
+	public static void main	(string[] args)
+	{
+		system.out.println("Hellow World")
+		
+	}
+}
---
+++ src/basics/VariableEx.java
@@ -0,0 +1,23 @@
+package basics;
+
+public class VariableEx {
+	private static short sNo;
+
+	public static void main(String args[])
+	{
+		int ticketPrice;
+		
+		var pi=3.14;
+		
+		sNo = 1;
+				
+		String name="Shan";
+		
+		ticketPrice=9000;
+		
+		System.out.println("ticketPrice");
+		
+		System.out.println("the price of the ticket is "+ticketPrice);
+	}
+
+}
